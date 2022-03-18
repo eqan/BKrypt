611f5b9f0685af233da8a8b4cf9e2833e0bcf9af
eqan <eqan.ahmad123@gmail.com>
2022-03-18 22:26:06 +0500

    Adding Solidity Transaction Features & More

---
+++ Client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x0A07cc1bf3d94fe00059dfD9835C3e404868e118';
--- Smart_Contract/hardhat.config.js
+++ Smart_Contract/hardhat.config.js
@@ -1,21 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/1-ceryD_8BuBoj7o8Ygciy3G5Czl8n50',
+      accounts: ['9406eb97ace8d76d7958a310051ecf9989f56f568c05012fa0b376add952e84b'],
+    },
+  },
 };
