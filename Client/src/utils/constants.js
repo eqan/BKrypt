6ae99d10c3d5b9f3f79e6774cf6653c492c88f30
eqan <eqan.ahmad123@gmail.com>
2022-03-19 16:22:26 +0500

    Bug Fixes

--- Client/src/utils/constants.js
+++ Client/src/utils/constants.js
@@ -1,4 +1,4 @@
-import abi from './Transactions.json';
+import abi from "./Transactions.json";
 
+export const contractAddress = "0x0A07cc1bf3d94fe00059dfD9835C3e404868e118";
 export const contractABI = abi.abi;
-export const contractAddress = '0x0A07cc1bf3d94fe00059dfD9835C3e404868e118';
